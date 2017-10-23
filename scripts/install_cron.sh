b0c3a1b14de03fd3c06abf190981db8f60e0d95d
Matthias Naber <naber.matthias@gmail.com>
2017-10-23 10:18:19 +0200

    run cron as user nginx

--- scripts/install_cron.sh
+++ scripts/install_cron.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euxo pipefail
 
 # todo: find out how to send emails (SES) and allow sending cron-results via email?
 # on how cron.hourly works: https://askubuntu.com/a/611430/389617
-cat << EOF > /etc/cron.hourly/99piwik
-/usr/bin/php /usr/share/nginx/piwik/console core:archive >> /var/log/piwik.core.archive.log
-EOF
-chmod +x /etc/cron.hourly/99piwik
+# cat << EOF > /etc/cron.hourly/99piwik
+# /usr/bin/php /usr/share/nginx/piwik/console core:archive >> /var/log/piwik.core.archive.log
+# EOF
+# chmod +x /etc/cron.hourly/99piwik
 
+# every hour : core:archive
+(crontab -u nginx -l 2>/dev/null; echo "45 * * * * /usr/bin/php /usr/share/nginx/piwik/console core:archive >> /var/log/piwik.core.archive.log") | crontab -u nginx -
+
+# every minute : queuedtracking:process
 (crontab -u nginx -l 2>/dev/null; echo "*/1 * * * * /usr/bin/php /usr/share/nginx/piwik/console queuedtracking:process >> /var/log/piwik.queuedtracking.process.log") | crontab -u nginx -
-
-EOF
-chmod +x ${SCRIPT}
