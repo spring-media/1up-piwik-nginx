fa61c9926490db595b57210502479d68557d3684
Matthias Naber <naber.matthias@gmail.com>
2018-01-24 12:51:54 +0100

    queued tracking: stderr to logs

--- scripts/install_cron.sh
+++ scripts/install_cron.sh
@@ -11,9 +11,9 @@ set -euo pipefail
 
 # every hour + some random seconds : core:archive
 # Warning: Resets the current crontab (on purpose)
-(echo "1,31 * * * * sleep \$(shuf -i 1-300 -n 1) ; /usr/bin/php /usr/share/nginx/piwik/console core:archive --php-cli-options='-d memory_limit=1536M' >> /var/log/nginx/piwik.core.archive.log") | crontab -u nginx -
+(echo "1,31 * * * * sleep \$(shuf -i 1-300 -n 1) ; /usr/bin/php /usr/share/nginx/piwik/console core:archive --php-cli-options='-d memory_limit=1536M' >> /var/log/nginx/piwik.core.archive.log 2>&1") | crontab -u nginx -
 
 # every minute : queuedtracking:process
-(crontab -u nginx -l 2>/dev/null; echo "*/1 * * * * /opt/piwik-nginx/scripts/queued_tracking.sh >> /var/log/nginx/piwik.queuedtracking.cron.log") | crontab -u nginx -
+(crontab -u nginx -l 2>/dev/null; echo "*/1 * * * * /opt/piwik-nginx/scripts/queued_tracking.sh >> /var/log/nginx/piwik.queuedtracking.cron.log 2>&1") | crontab -u nginx -
 
 echo -e "Current contents of /var/spool/cron/nginx: \nSTART\n$(cat /var/spool/cron/nginx)\nEND"
