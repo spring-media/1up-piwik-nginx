8b041c9d071ea1efa06ba496fff70bfffbd85207
Matthias Naber <naber.matthias@gmail.com>
2018-01-24 11:49:13 +0100

    improved queued tracking cron

--- scripts/install_cron.sh
+++ scripts/install_cron.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail
 
 # todo: find out how to send emails (SES) and allow sending cron-results via email?
 # on how cron.hourly works: https://askubuntu.com/a/611430/389617
@@ -8,11 +8,12 @@ set -euxo pipefail
 # EOF
 # chmod +x /etc/cron.hourly/99piwik
 
+
 # every hour + some random seconds : core:archive
 # Warning: Resets the current crontab (on purpose)
 (echo "1,31 * * * * sleep \$(shuf -i 1-300 -n 1) ; /usr/bin/php /usr/share/nginx/piwik/console core:archive --php-cli-options='-d memory_limit=1536M' >> /var/log/nginx/piwik.core.archive.log") | crontab -u nginx -
 
 # every minute : queuedtracking:process
-(crontab -u nginx -l 2>/dev/null; echo "*/1 * * * * /usr/bin/php /usr/share/nginx/piwik/console queuedtracking:process >> /var/log/nginx/piwik.queuedtracking.process.log") | crontab -u nginx -
+(crontab -u nginx -l 2>/dev/null; echo "*/1 * * * * /opt/piwik-nginx/scripts/queued_tracking.sh >> /var/log/nginx/piwik.queuedtracking.cron.log") | crontab -u nginx -
 
 echo -e "Current contents of /var/spool/cron/nginx: \nSTART\n$(cat /var/spool/cron/nginx)\nEND"
---
+++ scripts/queued_tracking.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+COMMAND="/usr/bin/php /usr/share/nginx/piwik/console queuedtracking:process"
+OUTPUT_PREFIX="/var/log/nginx/piwik.queuedtracking.process"
+
+CPU_COUNT=$(lscpu -p | egrep -v '^#' | wc -l)
+echo "CPU_COUNT = $CPU_COUNT"
+
+for i in $(seq 1 $CPU_COUNT); do
+    PIDFILE=/tmp/queuedtracking.$i.pid
+
+    if [ -f $PIDFILE ]; then
+        PID=$(cat $PIDFILE)
+        ps -p $PID > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo "Job $i is already running"
+            exit 1
+        else
+            ## Process not found assume not running
+            echo $$ > $PIDFILE
+            if [ $? -ne 0 ]; then
+                echo "Could not create PID file"
+                exit 1
+            fi
+        fi
+        else
+        echo $$ > $PIDFILE
+        if [ $? -ne 0 ]; then
+            echo "Could not create PID file"
+            exit 1
+        fi
+    fi
+    echo "Starting queued tracking $i"
+    START=$(date +%s.%N)
+    $COMMAND >> $OUTPUT_PREFIX.$i.log
+    END=$(date +%s.%N)
+    DIFF=$(echo "$END - $START" | bc)
+    echo "Finished queued tracking $i — took $DIFF seconds"
+    rm $PIDFILE
+done
