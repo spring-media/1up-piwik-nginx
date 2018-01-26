531bffbde7877cf71b6f19f99504f897cbd826e5
Matthias Naber <naber.matthias@gmail.com>
2018-01-26 12:46:03 +0100

    updated delete_logs script

---
+++ scripts/delete_logs.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+PIDFILE=/tmp/core:delete-logs.pid
+
+if [ -f $PIDFILE ]; then
+    PID=$(cat $PIDFILE)
+    ps -p $PID > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Job is already running"
+        exit 1
+    else
+        ## Process not found assume not running
+        echo $$ > $PIDFILE
+        if [ $? -ne 0 ]; then
+            echo "Could not create PID file"
+            exit 1
+        fi
+    fi
+    else
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]; then
+        echo "Could not create PID file"
+        exit 1
+    fi
+fi
+
+sleep $(shuf -i 1-3600 -n 1)
+START=$(date +%s.%N)
+/usr/bin/php /usr/share/nginx/piwik/console core:delete-logs-data \
+    --dates=2017-01-01,$(date --date='7 days ago' +'%Y-%m-%d') \
+    --optimize-tables \
+    --no-interaction \
+    --verbose >> /var/log/nginx/piwik.core.delete-logs.log
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo "`date +%F\ %T` Finished core:delete-logs — took $DIFF seconds"
+rm $PIDFILE
--- scripts/install_cron.sh
+++ scripts/install_cron.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 (echo "*/30 * * * * sleep \$(shuf -i 1-300 -n 1) ; /usr/bin/php /usr/share/nginx/piwik/console core:archive --php-cli-options='-d memory_limit=1536M' >> /var/log/nginx/piwik.core.archive.log 2>&1") | crontab -u nginx -
 
 # nightly delete old logs: https://matomo.org/faq/how-to/faq_20184/
-(crontab -u nginx -l 2>/dev/null; echo "17 0 * * * sleep $(shuf -i 1-3600 -n 1) ; /usr/bin/php /usr/share/nginx/piwik/console core:delete-logs-data --dates=2017-01-01,$(date --date='7 days ago' +'%Y-%m-%d') --optimize-tables --no-interaction -v >> /var/log/nginx/piwik.core.delete-logs.log 2>&1") | crontab -u nginx -
+(crontab -u nginx -l 2>/dev/null; echo "17 0 * * *  /opt/piwik-nginx/scripts/delete_logs.sh >> /var/log/nginx/piwik.core.delete-logs.cron.log 2>&1") | crontab -u nginx -
 
 # every minute : queuedtracking:process
 (crontab -u nginx -l 2>/dev/null; echo "*/1 * * * * /opt/piwik-nginx/scripts/queued_tracking.sh >> /var/log/nginx/piwik.queuedtracking.cron.log 2>&1") | crontab -u nginx -
