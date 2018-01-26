395feadcfd1374c2c7bd12f3b74084267406065d
Matthias Naber <naber.matthias@gmail.com>
2018-01-26 12:24:01 +0100

    added cron to delete old logs

--- scripts/install_cron.sh
+++ scripts/install_cron.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# todo: find out how to send emails (SES) and allow sending cron-results via email?
-# on how cron.hourly works: https://askubuntu.com/a/611430/389617
-# cat << EOF > /etc/cron.hourly/99piwik
-# /usr/bin/php /usr/share/nginx/piwik/console core:archive >> /var/log/nginx/piwik.core.archive.log
-# EOF
-# chmod +x /etc/cron.hourly/99piwik
-
 
 # every hour + some random seconds : core:archive
 # Warning: Resets the current crontab (on purpose)
-(echo "1,31 * * * * sleep \$(shuf -i 1-300 -n 1) ; /usr/bin/php /usr/share/nginx/piwik/console core:archive --php-cli-options='-d memory_limit=1536M' >> /var/log/nginx/piwik.core.archive.log 2>&1") | crontab -u nginx -
+(echo "*/30 * * * * sleep \$(shuf -i 1-300 -n 1) ; /usr/bin/php /usr/share/nginx/piwik/console core:archive --php-cli-options='-d memory_limit=1536M' >> /var/log/nginx/piwik.core.archive.log 2>&1") | crontab -u nginx -
+
+# nightly delete old logs: https://matomo.org/faq/how-to/faq_20184/
+(crontab -u nginx -l 2>/dev/null; echo "17 0 * * * sleep $(shuf -i 1-3600 -n 1) ; /usr/bin/php /usr/share/nginx/piwik/console core:delete-logs-data --dates=2017-01-01,$(date --date='7 days ago' +'%Y-%m-%d') --optimize-tables --no-interaction -v >> /var/log/nginx/piwik.core.delete-logs.log 2>&1") | crontab -u nginx -
 
 # every minute : queuedtracking:process
 (crontab -u nginx -l 2>/dev/null; echo "*/1 * * * * /opt/piwik-nginx/scripts/queued_tracking.sh >> /var/log/nginx/piwik.queuedtracking.cron.log 2>&1") | crontab -u nginx -
